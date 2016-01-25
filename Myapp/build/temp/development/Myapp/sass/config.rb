8aa6ccee74788f41c03db24fd0834fd73971e330
tmoore <tmoor039@fiu.edu>
2016-01-24 22:43:10 -0500

    dev env testing

---
+++ Myapp/build/temp/development/Myapp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/home/steven/bin/Sencha/Cmd/Myapp/build/.sass-cache'
---
+++ Myapp/build/temp/development/Myapp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /home/steven/bin/Sencha/Cmd/Myapp/app.js
